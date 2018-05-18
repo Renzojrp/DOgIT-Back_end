041c9c6c9e866bdb1d7d044ee66815a44a14930c
Renzojrp <1410327@utp.edu.pe>
2018-05-18 00:41:10 -0500

    Assistance

--- models/assistance.js
+++ models/assistance.js
@@ -5,9 +5,9 @@ const Schema = mongoose.Schema
 const User = mongoose.model('User')
 const Event = mongoose.model('Event')
 
-const EventSchema = new Schema({
+const AssistanceSchema = new Schema({
   user: {type: Schema.ObjectId, ref: "User"},
   event: {type: Schema.ObjectId, ref: "Event"}
 })
 
-module.exports = mongoose.model('Event', EventSchema)
+module.exports = mongoose.model('Assistance', AssistanceSchema)
